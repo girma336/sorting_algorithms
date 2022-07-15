9545f75c2983826eadc8a4ed66ddb26320968673
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-15 11:50:42 +0300

    all_new

--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+int len_size(listint_t *list);
 #endif
