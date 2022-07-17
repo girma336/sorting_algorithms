dad9251d276347fd366bb6f33bbe9048ef609ff9
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-17 11:41:14 +0300

    sort_h

--- sort.h
+++ sort.h
@@ -30,4 +30,6 @@ void quick_sort(int *array, size_t size);
 void quick_recursion(int *array, int start, int end, size_t size);
 int sort_pivot(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void support_list(listint_t **list, listint_t **nw);
 #endif
